b94df4a598722bb2e86fc59761bd4909f5d6c182
Meem Fatema <meemfatema95@gmail.com>
2024-05-14 04:13:11 -0400

    work on veiw all button in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,53 +9,54 @@ import { Fade } from "react-awesome-reveal";
 const Home = () => {
   const allData = useLoaderData();
   const featureFoods = allData.locationData;
-  const sixfeatureFoods = featureFoods.slice(0, 6);
-  console.log(allData);
 
+  // Sort the featureFoods by quantity in descending order
+  const sortedFeatureFoods = [...featureFoods].sort((a, b) => b.quantity - a.quantity);
+
+  // Select the top six feature foods
+  const sixfeatureFoods = sortedFeatureFoods.slice(0, 6);
 
   return (
-    <div className=" m-0 p-0">
+    <div className="m-0 p-0">
       <Helmet>
         <title> H-food | Home</title>
       </Helmet>
       <div>
-        <Slider></Slider>
+        <Slider />
       </div>
 
-      <div className=" max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto">
         <Fade duration="2000">
-          <section className="py-6 sm:py-12 ">
-            <div className=" p-6 mx-auto space-y-5">
+          <section className="py-6 sm:py-12">
+            <div className="p-6 mx-auto space-y-5">
               <div className="my-6 animate__animated animate__bounce">
-              <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Featured  <span className="text-lime-600">Food</span></h1>
+                <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+                  Featured <span className="text-lime-600">Food</span>
+                </h1>
               </div>
-              
+
               <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
                 {sixfeatureFoods.map((featureFood) => (
-                  <FeatureFood
-                    key={featureFood.id}
-                    cards={featureFood}
-                  ></FeatureFood>
+                  <FeatureFood key={featureFood.id} cards={featureFood} />
                 ))}
-              
               </div>
             </div>
             <div className="text-center">
-              <Link ></Link>
-                  <button className="btn btn-success btn-md">View All</button>
-                </div>
+              <Link to={'/available-food'} > <button className="btn  bg-lime-600 text-white btn-wide btn-md">View All</button></Link>
+             
+            </div>
           </section>
         </Fade>
       </div>
 
       <Fade direction="left" duration="2000">
         <div>
-          <Gallery></Gallery>
+          <Gallery />
         </div>
       </Fade>
       <Fade direction="left" duration="2000">
         <div>
-          <Review></Review>
+          <Review />
         </div>
       </Fade>
     </div>
